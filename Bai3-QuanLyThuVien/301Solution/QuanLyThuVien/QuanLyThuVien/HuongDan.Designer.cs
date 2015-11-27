737c45e806ccce225ab3acf0173229ae4726e503
NQTD <TCCLTD06@gmail.com>
2015-11-27 11:36:24 +0700

    Hoàn thiện form hướng dẫn bài 3
    Hoàn thiện form hướng dẫn bài 3

--- Bai3-QuanLyThuVien/301Solution/QuanLyThuVien/QuanLyThuVien/HuongDan.Designer.cs
+++ Bai3-QuanLyThuVien/301Solution/QuanLyThuVien/QuanLyThuVien/HuongDan.Designer.cs
@@ -28,19 +28,85 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.TreeNode treeNode1 = new System.Windows.Forms.TreeNode("Nhóm Thực Hiện");
+            System.Windows.Forms.TreeNode treeNode2 = new System.Windows.Forms.TreeNode("Giới Thiệu Sản Phẩm");
+            System.Windows.Forms.TreeNode treeNode3 = new System.Windows.Forms.TreeNode("Giới Thiệu Chung", new System.Windows.Forms.TreeNode[] {
+            treeNode1,
+            treeNode2});
+            System.Windows.Forms.TreeNode treeNode4 = new System.Windows.Forms.TreeNode("Quản Lý Chung");
+            System.Windows.Forms.TreeNode treeNode5 = new System.Windows.Forms.TreeNode("Sách");
+            System.Windows.Forms.TreeNode treeNode6 = new System.Windows.Forms.TreeNode("Bạn Đọc");
+            System.Windows.Forms.TreeNode treeNode7 = new System.Windows.Forms.TreeNode("NXB - Thể Loại");
+            System.Windows.Forms.TreeNode treeNode8 = new System.Windows.Forms.TreeNode("Phiếu Mượn");
+            System.Windows.Forms.TreeNode treeNode9 = new System.Windows.Forms.TreeNode("Hướng Dẫn Sử Dụng", new System.Windows.Forms.TreeNode[] {
+            treeNode4,
+            treeNode5,
+            treeNode6,
+            treeNode7,
+            treeNode8});
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
             this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.treeView1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.treeView1.Location = new System.Drawing.Point(13, 13);
+            this.treeView1.Name = "treeView1";
+            treeNode1.Name = "GT_NTH";
+            treeNode1.Text = "Nhóm Thực Hiện";
+            treeNode2.Name = "GT_GTSP";
+            treeNode2.Text = "Giới Thiệu Sản Phẩm";
+            treeNode3.Name = "GT";
+            treeNode3.Text = "Giới Thiệu Chung";
+            treeNode4.Name = "HD_QLC";
+            treeNode4.Text = "Quản Lý Chung";
+            treeNode5.Name = "HD_Sach";
+            treeNode5.Text = "Sách";
+            treeNode6.Name = "HD_BD";
+            treeNode6.Text = "Bạn Đọc";
+            treeNode7.Name = "HD_NXB";
+            treeNode7.Text = "NXB - Thể Loại";
+            treeNode8.Name = "HD_PM";
+            treeNode8.Text = "Phiếu Mượn";
+            treeNode9.Name = "HD";
+            treeNode9.Text = "Hướng Dẫn Sử Dụng";
+            this.treeView1.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            treeNode3,
+            treeNode9});
+            this.treeView1.Size = new System.Drawing.Size(197, 586);
+            this.treeView1.TabIndex = 0;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(217, 13);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(855, 586);
+            this.webBrowser1.TabIndex = 1;
             // 
             // HuongDan
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 14F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(314, 315);
+            this.ClientSize = new System.Drawing.Size(1084, 611);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.treeView1);
+            this.MaximumSize = new System.Drawing.Size(1100, 650);
+            this.MinimumSize = new System.Drawing.Size(1100, 650);
             this.Name = "HuongDan";
-            this.Text = "HuongDan";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Hướng Dẫn";
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
     }
 }
--- Bai3-QuanLyThuVien/301Solution/QuanLyThuVien/QuanLyThuVien/HuongDan.cs
+++ Bai3-QuanLyThuVien/301Solution/QuanLyThuVien/QuanLyThuVien/HuongDan.cs
@@ -16,5 +16,45 @@ namespace QuanLyThuVien
         {
             InitializeComponent();
         }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            string str;
+            if (treeView1.SelectedNode.Name == "GT_NTH")
+            {
+                str = Application.StartupPath + "\\Huong Dan\\Nhom thuc hien.mht";
+                webBrowser1.Navigate(str);
+            }
+            if (treeView1.SelectedNode.Name == "GT_GTSP")
+            {
+                str = Application.StartupPath + "\\Huong Dan\\Gioi thieu san pham.mht";
+                webBrowser1.Navigate(str);
+            }
+            if (treeView1.SelectedNode.Name == "HD_QLC")
+            {
+                str = Application.StartupPath + "\\Huong Dan\\Quan Ly Chung.mht";
+                webBrowser1.Navigate(str);
+            }
+            if (treeView1.SelectedNode.Name == "HD_BD")
+            {
+                str = Application.StartupPath + "\\Huong Dan\\Ban Doc.mht";
+                webBrowser1.Navigate(str);
+            }
+            if (treeView1.SelectedNode.Name == "HD_Sach")
+            {
+                str = Application.StartupPath + "\\Huong Dan\\Sach.mht";
+                webBrowser1.Navigate(str);
+            }
+            if (treeView1.SelectedNode.Name == "HD_NXB")
+            {
+                str = Application.StartupPath + "\\Huong Dan\\NXB - The Loai.mht";
+                webBrowser1.Navigate(str);
+            }
+            if (treeView1.SelectedNode.Name == "HD_PM")
+            {
+                str = Application.StartupPath + "\\Huong Dan\\Phieu Muon.mht";
+                webBrowser1.Navigate(str);
+            }
+        }
     }
 }
